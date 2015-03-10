df1810a8c2d94e088612d690bbbbbee09a633aec
Ria Carmin <akimalunar@gmail.com>
2015-03-10 13:46:41 -0700

    Initial setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Designer Store
+**design of an online store**
+v1
+
+**What I've used**
+- HTML5 Boilerplate (_modified_)
+- Sass with Bourbon
+- Neat
+- Bitters
+
+**Tested in:**
+- Mac OS: Chrome V.37
+- Mac OS: Safari V.10.9.4
---
+++ js/plugins.js
@@ -0,0 +1,51 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+(function (jQuery) {
+  jQuery.mark = {
+    jump: function (options) {
+      var defaults = {
+        selector: 'a.scroll-on-page-link'
+      };
+      if (typeof options == 'string') defaults.selector = options;
+      var options = jQuery.extend(defaults, options);
+      return jQuery(options.selector).click(function (e) {
+        var jumpobj = jQuery(this);
+        var target = jumpobj.attr('href');
+        var thespeed = 1000;
+        var offset = jQuery(target).offset().top;
+        jQuery('html,body').animate({
+          scrollTop: offset
+        }, thespeed, 'swing')
+        e.preventDefault();
+      })
+    }
+  }
+})(jQuery);
+
+
+jQuery(function(){  
+  jQuery.mark.jump();
+});
